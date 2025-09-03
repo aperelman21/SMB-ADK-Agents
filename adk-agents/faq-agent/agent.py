c823db56d267f262fdd6064411e55904f95c95ee
manufdzver <manufdzver@hotmail.com>
2025-09-02 19:46:55 -0600

    agente panadero

--- adk-agents/faq-agent/agent.py
+++ adk-agents/faq-agent/agent.py
@@ -1,18 +1,17 @@
-from zoneinfo import ZoneInfo
 from google.adk.agents import Agent
 from google.adk.tools import VertexAiSearchTool
 
-YOUR_DATASTORE_ID = "projects/my-custom-agents/locations/global/collections/default_collection/dataStores/jura-manual_1753826115104"
+YOUR_DATASTORE_ID = "projects/mx-summit-470817/locations/global/collections/default_collection/dataStores/mx-summit_1756863121472"
 vertex_search_tool = VertexAiSearchTool(data_store_id=YOUR_DATASTORE_ID)
 
 root_agent = Agent(
-    name="jura_assistant_agent",
+    name="panadero-asistant",
     model="gemini-2.0-flash",
     description=(
-        "Agent to answer questions about Jura E8 coffee machine."
+        "Agente que contesta cualquier pregunta acerca de cómo hacer pan."
     ),
     instruction=(
-        "You are a helpful agent who can answer user questions about the Jura E8 coffee machine."
+        "Eres un chef panadero que da clases acerca de cómo hacer pan y contestas cualquier duda acerca del proceso. Siempre contestale al usuario en el mismo idioma que ellos usen."
     ),
     tools=[vertex_search_tool],
 )
